3fa0770e4e1211046358037ac3c2ebc9f83066ec
standev <kvpash@gmail.com>
2023-01-08 14:27:42 -0500

    Updates script.js file. Refactors the code. Removed the residue code.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const fieldCont = document.querySelector(".game-field")
 const actionFld = document.querySelector(".action-field")
 const controllsPlace = document.querySelector(".controlls")
@@ -55,11 +54,6 @@ function represent(field){
 }
 
 
-represent(playerField)
-const numOfShips = {
-}
-
-
 // Input your ship on the field (Args are 1: the field 2: numerical coordinate (0-9) 3: literal coordinate (a-j))
 const setUpShip = (field, x, y) => {
     field[y][x] = "[_]|"
@@ -67,8 +61,7 @@ const setUpShip = (field, x, y) => {
 
 
 const generateFld = () => {
-    // need checker for existing ships
-    // refresh the game field in console
+    // Function for generating user's field in HTML
     represent(playerField)
     for (let j=0; j<=letters.length; j++) {
         for (let i=0; i<=10; i++) {
@@ -100,8 +93,6 @@ const generateFld = () => {
         div.style.backgroundColor = "#9ED5C5"
         div.classList.add("square-pf")
         div.setAttribute('id', `pf-j${j-1}i${i-1}`)
-        //event listener for situating the ships
-        // div.addEventListener("click", placeShip)
         }
         fieldCont.appendChild(div)
         }
@@ -119,7 +110,6 @@ const generateActionFld = () => {
     for (let j=0; j<=letters.length; j++) {
         for (let i=0; i<=10; i++) {
         const div = document.createElement('div')
-        
         div.style.height = "1rem"
         div.style.width = "1rem"
         div.style.display = "inline-block"
@@ -145,26 +135,8 @@ const generateActionFld = () => {
         div.style.border = "solid 1px black"
         div.style.marginRight = "1px"
         div.style.backgroundColor = "#9ED5C5"
-        div.setAttribute('id', `af-j${j-1}i${i-1}`)
+        div.setAttribute("id", `af-j${j-1}i${i-1}`)
         div.classList.add("square-af")
-        
-        //event listener for situating the ships
-        // div.addEventListener('click', () => {
-        //     if (actionField[letters[j-1]][i-1] === "[_]|"){
-        //         div.style.backgroundColor = "red"
-        //         actionField[letters[j-1]][i-1] = "[x]|"
-        //     } else if (actionField[letters[j-1]][i-1] === " _ |"){
-        //         actionField[letters[j-1]][i-1] = " o |"
-        //         //div.style.backgroundColor = "#635666"
-        //         div.innerHTML = "<h6 style='margin: 0.2rem .3rem'>X</h6>"
-        //         //setUpShip(actionField, i-1, letters[j-1])
-        //     }
-        
-
-        //     // need checker for existing ships
-        //     // refresh the game field in console
-        //     represent(actionField)
-        // })
         }
         actionFld.appendChild(div)
         }
@@ -211,63 +183,43 @@ const placeShip = () => {
 
 // Function for verification that cells where it is planned to situate ships are not occupied and for situating ships after that 
 const situateShip = (field, lenOfShip) => {
-    
-    console.log("SituateShip method starts")
-    console.log(field[letters[9]])
-    console.log(field[letters[10]])
-    // const shipPos = ["[_]"]
-    console.log(field[letters[0]][0])
-    console.log(field[letters[0]][9])
+    // Function for positioning and placing the ship
 
+    // Beginning coordinates for placing ship horizontally or vertically
     const xPos = Math.floor(Math.random()*(field[letters[0]].length-(lenOfShip-1)))
     const yPos = Math.floor(Math.random()*(field[letters[0]].length-(lenOfShip-1)))
-    //const yPos = y
-    console.log(xPos, yPos)
-    // const vertical = (Math.random()>=0.5)? 1:0
-    //console.log("vertical", vertical )
+
+    // Second constant coordinate wich depends on position will substitute x or y coordinate
     const ranNum = Math.floor(Math.random()*field[letters[0]].length)
     if (Math.random()>0.5) {
+        // Check when ship positioned vertically
         if (yPos === 0) {
             if (ranNum === 0) {
-                
                 for (let i=yPos; i<(yPos + lenOfShip+1); i++){
                     if(field[letters[i]][ranNum] !== " _ |" || field[letters[i]][ranNum+1] !== " _ |" ){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[i]][ranNum]}, ${field[letters[i]][ranNum+1]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
-
-
             } else if (ranNum === 9) {
                 for (let i=yPos; i<(yPos + lenOfShip+1); i++){
                     if(field[letters[i]][ranNum] !== " _ |" || field[letters[i]][ranNum-1] !== " _ |" ){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[i]][ranNum]}, ${field[letters[i]][ranNum-1]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
-
             } else {
                 for (let i=yPos; i<(yPos + lenOfShip+1); i++){
                     if(field[letters[i]][ranNum] !== " _ |" || field[letters[i]][ranNum-1] !== " _ |" || field[letters[i]][ranNum+1] !== " _ |"){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[i]][ranNum]}, ${field[letters[i]][ranNum-1]}, ${field[letters[i]][ranNum+1]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
-
             }
         } 
         // Checking that there are no ships in case when ship is situated close to "10" line of the field
         if ((yPos + lenOfShip) === 10) {
-            console.log("Second condition")
             // If ship is situated close to "1" line of the field
             if (ranNum === 0) {
                 for (let i=yPos-1; i<(yPos + lenOfShip); i++){
                     if(field[letters[i]][ranNum] !== " _ |" || field[letters[i]][ranNum+1] !== " _ |" ){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[i]][ranNum]}, ${field[letters[i]][ranNum+1]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
@@ -275,8 +227,6 @@ const situateShip = (field, lenOfShip) => {
             } else if (ranNum === 9) {
                 for (let i=yPos-1; i<(yPos + lenOfShip); i++){
                     if(field[letters[i]][ranNum] !== " _ |" || field[letters[i]][ranNum-1] !== " _ |" ){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[i]][ranNum]}, ${field[letters[i]][ranNum-1]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
@@ -284,8 +234,6 @@ const situateShip = (field, lenOfShip) => {
             } else {
                 for (let i=yPos-1; i<(yPos + lenOfShip); i++){
                     if(field[letters[i]][ranNum] !== " _ |" || field[letters[i]][ranNum-1] !== " _ |" || field[letters[i]][ranNum+1] !== " _ |"){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[i]][ranNum]}, ${field[letters[i]][ranNum-1]}, ${field[letters[i]][ranNum+1]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
@@ -293,17 +241,10 @@ const situateShip = (field, lenOfShip) => {
         }
 
         // Check if there are no ships when ship is planned to be situated in the middle of the field  between "A" and "J" field 
-
-        console.log(yPos + lenOfShip)
-        console.log(field[letters[yPos + lenOfShip]])
-
         if (yPos > 0 && (yPos + lenOfShip) <= 9) {
-            console.log("Third condition")
             if (ranNum === 0) {
                 for (let i=yPos-1; i<(yPos + lenOfShip+1); i++){
                     if(field[letters[i]][ranNum] !== " _ |" || field[letters[i]][ranNum+1] !== " _ |" ){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[i]][ranNum]}, ${field[letters[i]][ranNum+1]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
@@ -311,8 +252,6 @@ const situateShip = (field, lenOfShip) => {
             } else if (ranNum === 9) {
                 for (let i=yPos-1; i<(yPos + lenOfShip+1); i++){
                     if(field[letters[i]][ranNum] !== " _ |" || field[letters[i]][ranNum-1] !== " _ |" ){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[i]][ranNum]}, ${field[letters[i]][ranNum-1]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
@@ -320,8 +259,6 @@ const situateShip = (field, lenOfShip) => {
             } else {
                 for (let i=yPos-1; i<(yPos + lenOfShip+1); i++){
                     if(field[letters[i]][ranNum] !== " _ |" || field[letters[i]][ranNum-1] !== " _ |" || field[letters[i]][ranNum+1] !== " _ |"){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[i]][ranNum]}, ${field[letters[i]][ranNum-1]}, ${field[letters[i]][ranNum+1]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
@@ -335,37 +272,24 @@ const situateShip = (field, lenOfShip) => {
         }
 
     } else {
-
         // Checks when ships positioned horizontally
-        console.log(`ranNum = ${ranNum}`)
-        console.log(`xPos = ${xPos}`)
-
         // Check for empty cells when ships positioned on line "A"
- 
         if(ranNum === 0){
-            console.log("Technical message function line 0")
-
             if(xPos === 0) {
                 for (let i = xPos; i < (xPos + lenOfShip +1); i++){
                     if(field[letters[ranNum]][i] !== " _ |" || field[letters[ranNum+1]][i] !== " _ |"){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[ranNum]][i]}, ${field[letters[ranNum+1]][i]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
             }else if(xPos + lenOfShip === 9){
                 for (let i = (xPos-1); i < (xPos + lenOfShip+1); i++){
                     if(field[letters[ranNum]][i] !== " _ |" || field[letters[ranNum+1]][i] !== " _ |"){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[ranNum]][i]}, ${field[letters[ranNum+1]][i]}`)
                         return situateShip(field, lenOfShip)
                         }
                 }
             } else {
                 for (let i = (xPos-1); i < (xPos + lenOfShip +1); i++){
                     if(field[letters[ranNum]][i] !== " _ |" || field[letters[ranNum+1]][i] !== " _ |"){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[ranNum]][i]}, ${field[letters[ranNum+1]][i]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
@@ -373,31 +297,22 @@ const situateShip = (field, lenOfShip) => {
         }
 
         // Check for empty cells when ships positioned on line "J"
-
         if(ranNum === 9){
-            console.log("Technical message function line 9")
-
             if(xPos === 0) {
                 for (let i = xPos; i < xPos + lenOfShip +1; i++){
                     if(field[letters[ranNum]][i] !== " _ |" || field[letters[ranNum-1]][i] !== " _ |"){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[ranNum]][i]}, ${field[letters[ranNum-1]][i]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
             }else if((xPos + lenOfShip+1) === 9){
                     for (let i = xPos-1; i < xPos + lenOfShip +1; i++){
                         if(field[letters[ranNum]][i] !== " _ |" || field[letters[ranNum-1]][i] !== " _ |"){
-                            console.log("Cells are not empty. Try positioning again.")
-                            console.log(`${field[letters[ranNum]][i]}, ${field[letters[ranNum-1]][i]}`)
                             return situateShip(field, lenOfShip)
                             }
                     }
             } else {
                 for (let i = (xPos-1); i < (xPos + lenOfShip +1); i++){
                     if(field[letters[ranNum]][i] !== " _ |" || field[letters[ranNum-1]][i] !== " _ |"){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[ranNum]][i]}, ${field[letters[ranNum-1]][i]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
@@ -405,38 +320,27 @@ const situateShip = (field, lenOfShip) => {
         }
 
         // Check for empty cells when ships positioned between lines "A" and "J" not inclusive
-
         if(ranNum > 0 && ranNum < 9){
-            console.log("Technical message function lines between 0 and 9")
-
             if(xPos === 0) {
                 for (let i = xPos; i < (xPos + lenOfShip +1); i++){
                     if(field[letters[ranNum]][i] !== " _ |" || field[letters[ranNum-1]][i] !== " _ |" || field[letters[ranNum+1]][i] !== " _ |"){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[ranNum]][i]}, ${field[letters[ranNum-1]][i]}, ${field[letters[ranNum+1]][i]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
             }else if((xPos + lenOfShip) === 9){
                     for (let i = (xPos-1); i < (xPos + lenOfShip+1); i++){
                         if(field[letters[ranNum]][i] !== " _ |" || field[letters[ranNum-1]][i] !== " _ |" || field[letters[ranNum+1]][i] !== " _ |"){
-                            console.log("Cells are not empty. Try positioning again.")
-                            console.log(`${field[letters[ranNum]][i]}, ${field[letters[ranNum-1]][i]}, ${field[letters[ranNum+1]][i]}`)
                             return situateShip(field, lenOfShip)
                             }
                     }
             } else {
-                console.log("when ship is situated in the middle")
                 for (let i = (xPos-1); i < (xPos + lenOfShip +1); i++){
                     if(field[letters[ranNum]][i] !== " _ |" || field[letters[ranNum-1]][i] !== " _ |" || field[letters[ranNum+1]][i] !== " _ |" ){
-                        console.log("Cells are not empty. Try positioning again.")
-                        console.log(`${field[letters[ranNum]][i]}, ${field[letters[ranNum-1]][i]}, ${field[letters[ranNum+1]][i]}`)
                         return situateShip(field, lenOfShip)
                     }
                 }
             }
         }
-        console.log("It's horizontal")
         for (let i=xPos; i<(xPos + lenOfShip); i++){
              field[letters[ranNum]][i] = "[_]|"
         }
@@ -476,16 +380,8 @@ const placeShips = (field) => {
             }
         }
     } else {
-        console.log("Field is not empty")
         return false
     }
-}
-
-
-const initGame = () => {
-    //Here user should be prompted to situate ships
-    //number of ships will be checked and whole correctness of ship situation is checked here
-    //Computer's ships situation is generated here as well
 }
 
 
@@ -521,10 +417,7 @@ const turn = (player) => {
 }
 
 
-
-// Need function for computer's shooting
-
-
+// Game logic for the computer's shooting
 const shoot = (field) => {
     turn("Computer's")
     stopUserActions()
@@ -558,7 +451,6 @@ const shoot = (field) => {
 }
 
 
-
 const fieldReset = (field) => {
     for (let i=0; i<letters.length; i++){
         for (let j=0; j<field[letters[i]].length; j++){
@@ -568,6 +460,7 @@ const fieldReset = (field) => {
         }
     }
 }
+
 
 // Actual functionality to be added to actionField active elements
 const userActionHandler = (event) => {
@@ -592,6 +485,7 @@ const userActionHandler = (event) => {
     }
 }
 
+
 const stopUserActions = () => {
     // Stop accepting user action signals
     // Removing event Listeners from all the actionField active pieces
@@ -602,6 +496,7 @@ const stopUserActions = () => {
         }
     }
 }
+
 
 const gameover = (player) => {
     if (document.querySelector("h3")){
@@ -642,7 +537,6 @@ const gameover = (player) => {
 }
 
 
-
 const userShoot = () => {
     // Adding functionality and logic to actionField for user's interaction
     for (let j=0; j<letters.length; j++){
@@ -652,7 +546,6 @@ const userShoot = () => {
         }
     }
 }
-
 
 
 // battle function for deciding who is starting first
@@ -665,6 +558,7 @@ const battle = () => {
     userShoot()
 }
 
+
 const removeEvntListenerFromActField = () => {
     for (let j=0; j<letters.length; j++){
         for (let i=0; i<playerField[letters[j]].length; i++){
@@ -673,6 +567,7 @@ const removeEvntListenerFromActField = () => {
         }
     }
 }
+
 
 // Check that around hit ship there are other cells containing ship if not message destroyed should appear
 const readyForBattle = () => {
@@ -696,6 +591,7 @@ const readyForBattle = () => {
     controllsPlace.appendChild(button)
 }
 
+
 const showMessage2 = (message, timeOut) => {
     setTimeout(() => {
         if (document.querySelector("h3")){
@@ -708,6 +604,7 @@ const showMessage2 = (message, timeOut) => {
     }, timeOut)
 }
 
+
 const placeYourShipsMessage = () => {
         showMessage2("Place your ships", 1000)
         showMessage2("1 x Battleship 4 cells", 3000)
@@ -716,6 +613,7 @@ const placeYourShipsMessage = () => {
         showMessage2("4 x Destroyers 1 cell", 12000)
         showMessage2("Good luck!", 15000)
 }
+
 
 const game = () => {
     placeYourShipsMessage()
